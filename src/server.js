2653eb7adc3c371d88724aee4f6d23dd70459008
Randy Douglas <randy.e.douglas@gmail.com>
2021-01-24 14:59:49 -0500

    init

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const app = require('./app')
+const PORT = process.env.PORT || 3000;
+
+app.get('/api/*', (req, res) => {
+  res.json({ok: true});
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));

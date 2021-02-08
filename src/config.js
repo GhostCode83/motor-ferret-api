8d65dd8ddb15f385fa3c789bec867066620b1ad4
Randy Douglas <randy.e.douglas@gmail.com>
2021-02-08 12:22:45 -0500

    pg version change and ssl update

--- src/config.js
+++ src/config.js
@@ -1,9 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  // DATABASE_URL: process.env.DATABASE_URL || 'postgresql://ferret_admin@localhost/motor_ferret',
-  DATABASE_URL: process.env.DATABASE_URL || 'postgres://dtbcetevkdtznw:5fff5482c64c3f8b9fa42830fd0b7a45a831b0e813e6f7c26bf629addfa1b70b@ec2-54-162-119-125.compute-1.amazonaws.com:5432/dasrgjas97a1gi?ssl=true',
-
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://ferret_admin@localhost/motor_ferret',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://ferret_admin@localhost/motor_ferret_test',
   JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret'
 }
--- src/server.js
+++ src/server.js
@@ -4,6 +4,9 @@ const knex = require('knex')
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config');
 
+const pg = require('pg');
+pg.defaults.ssl = process.env.NODE_ENV === "production";
+
 // app.get('/api/*', (req, res) => {
 //   res.json({ ok: true });
 // });
@@ -11,7 +14,6 @@ const { PORT, DATABASE_URL } = require('./config');
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
-  ssl: true,
 })
 
 app.set('db', db)
